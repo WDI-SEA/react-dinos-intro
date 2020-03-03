64f661558515f57d571c2b7e012cc61d2c48aa26
John William Davis <jwdavisdeveloper@gmail.com>
2020-03-03 10:30:31 -0800

    changes after code-along

--- dino-blog/src/Post.js
+++ dino-blog/src/Post.js
@@ -1,52 +1,135 @@
 import React, {Component} from "react";
+import './App.css';
 
-let dinos = {
-  postTitle: "Dinosaurs are awesome",
-  author: "by Stealthy Stegosaurus",
-  content: "Check out this body property!",
-  comments: "Comments:"
+
+
+
+let elems = {
+  header: 'Dinos be dinoin',
+  author: 'Richard Scary',
+  content: 'This is a post about dinos listen up people!'
 }
 
-class PostTitle extends Component {
-  render() {
-    return <h1>{this.props.postTitle}</h1>
+let commentStuff = [
+  {
+    commentHeader: 'Rawr',
+    commentBody: 'Dinobama says hi'
+  },
+  {
+    commentHeader: 'Radasaurus',
+    commentBody: 'Rawrxd'
   }
-}
+];
 
-class Author extends Component {
-  render() {
-    return <p>{this.props.author}</p>
-  }
-}
-
-class Content extends Component {
-  render() {
-    return <p>{this.props.content}</p>
-  }
-}
-
-
-class Comments extends Component {
-  render() {
-    return <p>{this.props.comments}</p>
-  }
-}
-
-class Post extends Component {
+class Header extends Component {
   render() {
     return(
       <div>
-        <PostTitle postTitle={dinos.postTitle} />
-        <Author author={dinos.author} />
-        <Content content={dinos.content} />
-        <Comments comments={dinos.comments} />
+      <h1>{this.props.header}</h1>
+      <h3>{this.props.author}</h3>
       </div>
     )
   }
 }
 
-export default function App() {
-  return (
-    <Post />
-  );
+class Content extends Component {
+  render() {
+    return (
+      <p>{this.props.content}</p>
+    )
+  }
 }
+
+class Comment extends Component {
+  render() {
+    return (
+      <div>
+      <h3>{this.props.commentHeader}</h3>
+      <p>{this.props.commentBody}</p>
+    </div>
+    )
+  }
+}
+
+class Commentpost extends Component {
+  render() {
+    let comments = [];
+    commentStuff.forEach(function(comment, index) {
+     comments.push( 
+     <Comment 
+     commentHeader={comment.commentHeader} 
+     commentBody={comment.commentBody}/>);
+    })
+    return (
+      <div>
+        {comments}
+      </div>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Header header={elems.header} author={elems.author}/>
+      <Content content={elems.content}/>
+      <Commentpost />
+
+    </div>
+  )
+}
+
+export default App;
+
+// ---------------------------------
+
+// let dinos = {
+//   postTitle: "Dinosaurs are awesome",
+//   author: "by Stealthy Stegosaurus",
+//   content: "Check out this body property!",
+//   comments: "Comments:"
+// }
+
+// class PostTitle extends Component {
+//   render() {
+//     return <h1>{this.props.postTitle}</h1>
+//   }
+// }
+
+// class Author extends Component {
+//   render() {
+//     return <p>{this.props.author}</p>
+//   }
+// }
+
+// class Content extends Component {
+//   render() {
+//     return <p>{this.props.content}</p>
+//   }
+// }
+
+
+// class Comments extends Component {
+//   render() {
+//     return <p>{this.props.comments}</p>
+//   }
+// }
+
+// class Post extends Component {
+//   render() {
+//     return(
+//       <div>
+//         <PostTitle postTitle={dinos.postTitle} />
+//         <Author author={dinos.author} />
+//         <Content content={dinos.content} />
+//         <Comments comments={dinos.comments} />
+//       </div>
+//     )
+//   }
+// }
+
+// export default function App() {
+//   return (
+//     <Post />
+//   );
+// }
