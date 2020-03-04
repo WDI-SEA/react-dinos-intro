a380b769a9710e3efd9abc4173b5d542d942a299
melissay94 <melissay94@yahoo.com>
2020-03-03 19:37:36 -0800

    You can change the post body now

--- src/Post.js
+++ src/Post.js
@@ -3,6 +3,19 @@ import Comment from "./Comment.js";
 
 class Post extends Component {
 
+    constructor(props) {
+        super();
+        this.state = {
+            body: props.body
+        }
+    }
+
+    changeBody(e) {
+        let userInput = prompt("What body do you want?");
+        this.setState({
+            body: userInput
+        });
+    }
     
     render() {
         const comments = [];
@@ -14,7 +27,8 @@ class Post extends Component {
             <div>
                 <h1 className="post-title">{this.props.title}</h1>
                 <h3 className="post-author">by {this.props.author}</h3>
-                <h3 className="post-content">{this.props.body}</h3>
+                <h3 className="post-content">{this.state.body}</h3>
+                <button onClick={e => this.changeBody(e)}>Surprise!</button>
                 <h2 className="post-comment-title">Comments:</h2>
                 <div className="post-comment">{comments}</div>
             </div>
