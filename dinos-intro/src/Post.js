7f09edce3bbcda3437b4c8052ed706b89e2aa398
shawhien <shawhien@gmail.com>
2020-04-14 12:35:26 -0700

    adding to github

---
+++ dinos-intro/src/Post.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import "./App.css";
+
+let elems = {
+    title: "Dinosaurs are dinotastic",
+    author: "Shawhien",
+    content: ["Text text text text!"],
+    comments: ["Here we are", "Dinossssss", "Againnnnn"]
+};
+
+class Title extends Component {
+    render() {
+        return <h1> {this.props.title} </h1>;
+    }
+}
+
+class Author extends Component {
+    render() {
+        return (<p>{this.props.author} </p>);
+    }
+}
+
+class Content extends Component {
+    render() {
+        let content = this.props.content.map(x => {
+            return (<p> {x} </p>);
+        });
+        return <p>{content}</p>
+    }
+}
+
+class Comments extends Component {
+    render() {
+        let comment = this.props.comments.map(x => {
+            return (<p> {x} </p>);
+        });
+        return (
+            <div>
+                <h2 > Comments: </h2>
+                <p> {comment} </p>
+            </div>
+        );
+    }
+}
+
+class PostHandle extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          body: this.props.content
+        }
+        this.changeContent = this.changeContent.bind(this)
+      }
+    
+      changeContent(e) {
+        // render alert on click
+        let answer = prompt("What would you like to update to?")
+    
+        //update state
+        this.setState((prevState, props) => {
+          return {
+            body: answer
+          }
+        })
+      }
+    
+        render () {
+          return (
+            <div>
+              <p>{this.state.body}</p>
+              <button onClick= {this.changeContent}>Edith Body</button>
+              </div>
+                
+            )
+       }
+
+}
+
+class Post extends Component {
+    render() {
+        return (
+            <div class="App">
+                <Title title={elems.title} />
+                <Author author={elems.author} />
+                <Content content={elems.content} />
+                <PostHandle />
+                <Comments comments={elems.comments} />
+            </div>
+        );
+    }
+}
+
+export default function App() {
+    return <Post />;
+}
