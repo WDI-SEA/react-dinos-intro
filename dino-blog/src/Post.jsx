cbdc359168a35a2eb9d547d9af17d6ed625d97cf
Weston Bailey <void@MBP.local>
2020-07-15 15:24:36 -0700

    made post componenet

---
+++ dino-blog/src/Post.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Post Title</h1>
+        <h5>Post Author</h5>
+        <p>Post Content</p>
+        <h3>Comments:</h3>
+        <p>comment text</p>
+      </div>
+    );
+  }
+}
+
+export default Post;
