7994618271eeb92097245d77806a79ef55814e85
Nick Hvattum <nhvattum@gmail.com>
2020-07-15 16:47:39 -0700

    finished

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.postTitle}</h1>
+                <h3>by {this.props.author}</h3>
+                <p>{this.props.content}</p>
+                <h2>Comments:</h2>
+                <p>{this.props.comments}</p>
+            </div>
+        );
+    }
+}
+
+export default Post;
