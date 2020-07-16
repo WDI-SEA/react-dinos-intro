68a89a7ef53c4d8b6c439f6992ba1a73da30943d
sinav <sinavameghi@gmail.com>
2020-07-15 17:02:37 -0700

    that was quick

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Post from "./Post"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        < Post />
+      </div>
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Post.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+let post = {
+    header: "Some Dino based Blog Post",
+    author: "SEIR 526 Class",
+    content: "Some freaking brontosaurus",
+    comment: "This is how I really feel about dinos 🦖"
+}
+
+
+
+class Comment extends Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.comment}</p>
+            </div>
+        );
+    }
+}
+
+class Content extends Component {
+    render () {
+        return (
+            <p>{this.props.content}</p>
+        )
+    }
+}
+
+class Header extends Component {
+    render () {
+        return (
+            <h1>{this.props.header}</h1>
+        )
+    }
+}
+
+class Author extends Component {
+    render () {
+        return (
+            <h3>{this.props.author}</h3>
+        )
+    }
+}
+
+class Post extends Component {
+    render () {
+        return (
+            <div>
+                <Header header={post.header} />
+                <Author author={post.author} />
+                <Content content={post.content} />
+                <Comment comment={post.comment} />
+            </div>
+        )
+    }
+}
+
+
+
+
+export default Post;
