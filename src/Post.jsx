193cbd3eec4c623bbbc57606a43d6de087c0684e
Weston Bailey <void@MBP.local>
2020-07-16 10:52:16 -0700

    converted Post component to function

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,30 @@
+// importing {  Component } is only needed for the class component
 import React, { Component } from 'react';
 
-class Post extends Component {
+// using a functional component to render posts
+// removed this. and passed an argument of props
+const Post = (props) => {
+  const comments = props.post.comments.map((comment, key) => {
+    return <p key={ key }>{ comment }</p>
+  })
+  return (
+    <div>
+      <hr />
+      <h1>{ props.post.title }</h1>
+      <h5>{ props.post.author }</h5>
+      <p>{ props.post.content }</p>
+      <h3>Comments:</h3>
+      { comments }
+      <hr />
+    </div>
+  )
+}
+
+// old post is written as a class component
+class PostOld extends Component {
   render() {
     const comments = this.props.post.comments.map((comment, key) => {
-      return <p key={ key }>{ comment  }</p>
+      return <p key={ key }>{ comment }</p>
     })
     return (
       <div>
