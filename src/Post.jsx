0dd5eda5d32fd869108da11bef6afadb18ddd8a1
Paul <pdpadian@gmail.com>
2020-07-16 11:54:03 -0600

    functional refactor

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, {} from 'react';
 
-class Post extends Component {
-    render() {
-        console.log(this.props.post[0])
-        console.log(this.props.post[0].title)
-        return (
-            <div>
-                <h1>{this.props.post[0].title}</h1>
-                <h2>By {this.props.post[0].author}</h2>
-                <p>{this.props.post[0].content}</p>
-                <h1>Comments:</h1>
-                <h5>{this.props.post[0].comment}</h5>
-            </div>
-        );
-    }
+const Post = (props) => {
+    return (
+
+        props.post.map((blog) => 
+        <div>
+            <h1>{blog.title}</h1>
+            <h2>by {blog.author}</h2>
+            <p>{blog.content}</p>
+            <h1>Comments:</h1>
+            <h5>{blog.comment}</h5>
+        </div>)
+      
+    );
 }
 
 export default Post;
