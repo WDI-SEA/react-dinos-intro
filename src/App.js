0141dea80c1e1b6a4dd644054d081e77039bf146
jdephil <jennifer.dephillips@gmail.com>
2020-07-16 11:57:54 -0400

    in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Post from './Post'
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let details = [
+  {
+  'postTitle': 'Dinosaurs are awesome',
+  'author': 'Stealthy Stegosaurus',
+  'content': 'Check out this body property',
+  'comments': 'First'
+  }
+]
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <Post title={details.postTitle} 
+        author={details.author} 
+        content={details.content}
+        comments={details.comments} />
+      </div>
+    );
+  }
 }
 
 export default App;
