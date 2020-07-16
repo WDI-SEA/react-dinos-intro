3942fa8a9c1552a44b1ee06137bda691e27a1d5d
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-07-16 01:30:15 -0500

    Created Post and added values for title,content,author, and comments

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import './App.css';
 import Post from './Post';
 
 function App() {
+
+  let content = [
+    "Dinosaurs are super cool",
+    "Dinosaur fossils have been found on all seven continents.",
+    "The name \"Velociraptor\" means speedy thief",
+    "Dinosaurs dominated Earth for over 165 million years. Humans have been around for only 2 million years."
+  ]
+
+  let comments = [
+    "First!",
+    "Wowww Dinosaurs are cool af",
+    "Jeff Goldblum is bae",
+    " ^ Okay, let's keep it PG"
+  ]
+
   return (
     <div className="App">
-      <Post />
+      <Post 
+        postTitle={'Dinosaurs are awesome'}
+        author={'Stealthy Stegosaurus'}
+        content={content}
+        comments={comments}
+      />
     </div>
   );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 
 class Post extends Component {
     render() {
+
+        let allContent = this.props.content.map((entry) => {
+            return (
+                <div>
+                    {entry}
+                </div>
+            )
+        } )
+        
+        let allComments = this.props.comments.map((comment) => {
+            return (
+                <div>
+                    {comment}
+                </div>
+            )
+        })
+
         return (
-            <div>
-                <h1>Dinosaurs are awesome</h1>
+            <div className="react-dinos">
+                <h1>{this.props.postTitle}</h1>
+                <p>by {this.props.author}</p>
+                <p> {allContent} </p>
+
+                <div className="comments">
+                    <h3>Comments:</h3>
+                    <p> {allComments} </p>
+                </div>
             </div>
         );
     }
