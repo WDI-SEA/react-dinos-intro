7e0d148fe7b57145925cacca9ea6c0fe58728b3f
sky777666 <lmyer12@my.wgu.edu>
2020-07-16 10:54:49 -0700

    Luke Myers - done

--- dinoreact/src/Post.js
+++ dinoreact/src/Post.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-
-
-// class Post extends Component {
-//     constructor(props){
-//       super(props)
-//     this.state = {
-//       author:props.author,
-//       author: 'By: Luke A. Myers'
-//     }
-//   }
-
-class Post extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Dinosaurs are awesome</h1>
-                <p>By: Luke A. Myers</p>
-                <p>Dinosaurs have been my favorite animal for my whole childhood. I knew at least 15 types by name when I was only 6 or 7 years old. In the 20 + years that have passed science has learned much more about them. We know how they died off. We know that some if not many might have had fury feathers. We also know that birds come dirrectly from Dinosaurs.</p>
-                <h4>Comments:</h4>
-                <p>First Comment. This blog is so cool!</p>
-                
-            </div>
-        );
-    }
+import React, {} from 'react';
+let post = {
+    header: "Some Dino based Blog Post",
+    author: "SEI 29 Class",
+    content: "Some freaking brontosaurus",
+    comment: "This is how I really feel about dinos 🦖"
 }
+const Comment = (props) => {
+    return (
+        <div>
+            <p>{props.comment}</p>
+        </div>
+    )
+}
+const Content = (props) => {
+    return (
+        <p>{props.content}</p>
+    )
+}
+const Header = (props) => {
+    return (
+        <h1>{props.header}</h1>
+    )
+}
+const Author = (props) => {
+    return (
+        <h3>{props.author}</h3>
+    )
+}
+const Post = () => {
+    return (
+        <div>
+            <Header header={post.header} />
+            <Author author={post.author} />
+            <Content content={post.content} />
+            <Comment comment={post.comment} />
+        </div>
+    )
+}
+const App = () => {
+  return (
+    <Post />
+  )
+}
+export default App;
 
-export default Post;
+
+
+
+
